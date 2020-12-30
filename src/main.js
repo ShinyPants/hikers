9f49cf5a3c2346b79742bd79d774b619e3554f0f
ShinyPants <2453812362@qq.com>
2020-12-30 14:22:20 +0800

    axios配置

--- src/main.js
+++ src/main.js
@@ -5,9 +5,16 @@ import router from './router'
 // 引入Element
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 引入axios
+import axios from 'axios'
 
 // 使用ElementUI
 Vue.use(ElementUI)
+
+// 将axios放入全局变量，将来需要时可用this.$axios
+Vue.prototype.$axios = axios
+// 设置axios的baseUrl
+axios.defaults.baseURL = "http://127.0.0.1:8081"
 
 Vue.config.productionTip = false
 
